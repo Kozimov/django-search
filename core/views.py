e9939a1a171edf4f028f63e62c80d7eb0686fb46
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-28 12:10:29 +0500

    last day filter name search

--- core/views.py
+++ core/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from .models import Works
 
 def home(request):
-    works = Works.objects.all()
+    if 'search' in request.GET:
+        search_word = request.GET['search']
+        works = Works.objects.filter(name__icontains=search_word)
+    else:
+        works = Works.objects.all()
+
     context = {
         "works": works
     }
-
     return render(request, 'index.html', context)
