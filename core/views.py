d8fda476dfd26ed026f54957c4adb0c125f76cc4
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-28 12:30:35 +0500

    search all

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Works
+from django.db.models import Q
 
 def home(request):
     if 'search' in request.GET:
         search_word = request.GET['search']
-        works = Works.objects.filter(name__icontains=search_word)
+        full = Q(Q(name__icontains=search_word) | Q(surname__icontains=search_word) | Q(age__icontains=search_word) | Q(job__icontains=search_word))
+        works = Works.objects.filter(full)
     else:
         works = Works.objects.all()
 
